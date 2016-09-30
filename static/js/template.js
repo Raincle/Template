b768dce6e7b8078587648348dd41b5f0c965a7b6
Raincle <raincle.hong@arkdesign.cn>
2016-09-30 10:59:57 +0800

    test

--- static/js/template.js
+++ static/js/template.js
@@ -9,7 +9,14 @@ $(function() {
 
 
     $('.btn').click(function() {
-        window.location.href = "https://taoquan.taobao.com/coupon/unify_apply.htm?sellerId=2986103280&activityId=27efdeb9e97b4f52854f6cf3da901d73";
+        $('.tipWrapper').css({display:'block'}).animate({opacity:1},'fast');
+        //window.location.href = "https://taoquan.taobao.com/coupon/unify_apply.htm?sellerId=2986103280&activityId=27efdeb9e97b4f52854f6cf3da901d73";
+    });
+
+    $('.tipWrapper').click(function() {
+        $(this).animate({opacity:0},'fast',function() {
+            $(this).css({display:'none'})
+        });
     });
 
 });
